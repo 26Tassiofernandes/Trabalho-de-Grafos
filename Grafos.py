2502908a1947befaf07917d5495e81c3e867b89b
Tássio <86972667+26Tassiofernandes@users.noreply.github.com>
2022-10-21 10:44:00 -0300

    Update Grafos.py

--- Grafos.py
+++ Grafos.py
@@ -1,8 +1,8 @@
 ## Problema das sarnas
 
-- Neste problema temos de encontrar a árvore de suporte de custo mínimo que liga as sardas.
-- Para saber a distâcia da sarda(x1,y1) a (x2,y2) , basta usar √(x1-x2)² + (y1-y2)²
-- Tanto pode usar o algoritmo de Prim, como o algoritmo de Kruskal. Uma sugestão é implementar ambos e verificar que dão a mesma resposta.
+# Neste problema temos de encontrar a árvore de suporte de custo mínimo que liga as sardas.
+# Para saber a distâcia da sarda(x1,y1) a (x2,y2) , basta usar √(x1-x2)² + (y1-y2)²
+# Tanto pode usar o algoritmo de Prim, como o algoritmo de Kruskal. Uma sugestão é implementar ambos e verificar que dão a mesma resposta.
 
 # Vamos representar cada sarda (vértice) por um dicionário com suas respectivas coordenadas:
 
@@ -20,21 +20,21 @@ distancia_1_2 = sqrt(dist_sd_1_2) # calcula raiz quadrada
 print(f'A distância entre os 2 pontos {distancia_1_2:.3f}')
 
 
-- Sarda 1 e Sarda 3:
+# Sarda 1 e Sarda 3:
 
 dist_sd_1_3 = (sarda_1['coord_x1'] - sarda_3['coord_x3']) ** 2 + (sarda_1['coord_y1'] - sarda_3['coord_y3']) ** 2
 distancia_1_3 = sqrt(dist_sd_1_3)
 print(f'A distância entre os 2 pontos {distancia_1_3:.3f}')
 
 
--  Sarda 2 e Sarda 3:
+# Sarda 2 e Sarda 3:
 
 dist_sd_2_3 = (sarda_2['coord_x2'] - sarda_3['coord_x3']) ** 2 + (sarda_3['coord_y3'] - sarda_2['coord_y2']) ** 2
 distancia_2_3 = sqrt(dist_sd_2_3)
 print(f'A distância entre os 2 pontos {distancia_2_3:.3f}')
 
 
-- Algoritmo de Kruskal para achar a árvore geradora mínima
+# Algoritmo de Kruskal para achar a árvore geradora mínima
 
 floresta = [sarda_1, sarda_2, sarda_3] # Meus vértices (sardas)
 conjunto = [distancia_1_2, distancia_1_3, distancia_2_3]
